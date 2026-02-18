23ed9b300b7aac5e7ce3eed108d387ac8e84ade7
Mohas <Mohasadio2002@gmail.com>
2026-02-18 00:20:52 +0000

    swagger

--- src/main/java/com/esmt/cartographiegestionprojet/controller/UserController.java
+++ src/main/java/com/esmt/cartographiegestionprojet/controller/UserController.java
@@ -3,6 +3,7 @@ package com.esmt.cartographiegestionprojet.controller;
 import com.esmt.cartographiegestionprojet.dto.UserCreateDTO;
 import com.esmt.cartographiegestionprojet.dto.UserDTO;
 import com.esmt.cartographiegestionprojet.dto.response.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -15,6 +16,7 @@ import java.util.UUID;
 
 @RestController
 @RequestMapping("/api/users")
+@Tag(name = "Users",description = "Gestion des utilisateurs")
 public class UserController {
 
     @Autowired
